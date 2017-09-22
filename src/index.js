7373e4a2feef2bc09187eb2a0845b5e93ae2031c
Kornelia Szabo <kszabo@thoughtworks.com>
2017-09-22 08:53:56 +0100

    Game calculates winner and stops you from ticking a field more than once. Also game stops when there is a winner

--- src/index.js
+++ src/index.js
@@ -34,6 +34,10 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    
     squares[i] = this.whoIsNext();
     this.setState({
       squares: squares,
@@ -49,7 +53,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.whoIsNext());
+    const winner = calculateWinner(this.state.squares);
+    let status;
+
+    if (winner) {
+      status = 'Winner is ' + winner + '! Yassss!';
+    } else {
+      status = 'Next player: ' + (this.whoIsNext());
+    }
 
     return (
       <div className="fieldHeader">
@@ -77,18 +88,40 @@ class Board extends React.Component {
 
 class Game extends React.Component {
   render() {
+
     return (
       <div className="game">
         <div className="game-board">
           <Board/>
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
-    );
+    )
+      ;
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
